6d0aaaf7e99abf8d072498ae13e65d7213b8496f
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2025-02-18 15:33:31 +0000

    removed not needed logs

--- redux-project/src/app/page.jsx
+++ redux-project/src/app/page.jsx
@@ -2,44 +2,22 @@
 
 import styles from "./page.module.css";
 import { useEffect } from "react";
-import { useAppSelector } from "./redux/store";
 import { useDispatch } from "react-redux";
 import { fetchStocks } from "./redux/features/stocksSlice";
+import StockDataGraph from "./components/StockDataGraph";
 
 export default function Home() {
   const dispatch = useDispatch();
-  const { stocksData, hasError, isLoading } = useAppSelector(
-    (state) => state.stocksData
-  );
-  // const metaData = stocksData["Time Series (Daily)"];
-  const metaData = stocksData["Time Series (Daily)"] || {};
-
-  console.log("stocks data 2", stocksData);
 
   useEffect(() => {
     dispatch(fetchStocks());
   }, []);
 
-  if (hasError) {
-    return <div>Error fetching data</div>;
-  }
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.ctas}>
-          <ul>
-            {stocksData &&
-              Object.entries(metaData).map(([key, value]) => (
-                <li style={{ color: "white" }} key={key}>
-                  <strong>{key}:</strong> {JSON.stringify(value)}
-                </li>
-              ))}
-          </ul>
+          <StockDataGraph />
         </div>
       </main>
     </div>

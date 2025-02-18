64166a03aa980a3a89d7e111ff4b5516717fdde9
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2025-02-18 15:33:16 +0000

    created component for stock data graph

---
+++ redux-project/src/app/components/StockDataGraph.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useAppSelector } from "../redux/store";
+
+const StockDataGraph = () => {
+  const { stocksData, hasError, isLoading } = useAppSelector(
+    (state) => state.stocksData
+  );
+
+  if (hasError) {
+    return <div>Error fetching data</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const metaData = stocksData["Time Series (Daily)"] || {};
+  return (
+    <div>
+      {stocksData &&
+        Object.entries(metaData).map(([key, value]) => (
+          <li style={{ color: "white" }} key={key}>
+            <strong>{key}:</strong> {JSON.stringify(value)}
+          </li>
+        ))}
+    </div>
+  );
+};
+
+export default StockDataGraph;
